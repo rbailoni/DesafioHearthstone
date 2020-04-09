0f08ad22ebaa3e5ed32bd54ae70f107aee812f88
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2020-04-09 19:09:13 -0300

    Cria classe que recebe o Response do endpoint Cards.

---
+++ DesafioHearthstone/Response/CardsResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CardsResponse.swift
+//  DesafioHearthstone
+//
+//  Created by Ricardo Bailoni on 09/04/20.
+//  Copyright © 2020 Ricardo Bailoni. All rights reserved.
+//
+
+import Foundation
+
+struct CardsResponse: Codable {
+    let img: String?
+}
