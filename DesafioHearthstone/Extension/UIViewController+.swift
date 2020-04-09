19e2cddd241d8418731b2a65ed321aab1aab9178
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2020-04-09 15:14:16 -0300

    Cria extension da UIViewController.

---
+++ DesafioHearthstone/Extension/UIViewController+.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+.swift
+//  DesafioHearthstone
+//
+//  Created by Ricardo Bailoni on 09/04/20.
+//  Copyright © 2020 Ricardo Bailoni. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showError(error: ResponseError) {
+        let alert = UIAlertController(title: "Erro", message: error.localizedDescription, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
